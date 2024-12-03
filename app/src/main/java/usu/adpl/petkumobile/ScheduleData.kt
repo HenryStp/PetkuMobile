6eaa0bf6dfdc857ac2d895fb2e93da55270c46d6
ElizabethSrgh <saragihelizabeth28@gmail.com>
2024-12-03 15:51:34 +0700

    calender & reminder

---
+++ app/src/main/java/usu/adpl/petkumobile/ScheduleData.kt
@@ -0,0 +1,10 @@
+package usu.adpl.petkumobile
+
+
+data class ScheduleData(
+    val day: String = "",
+    val date: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val backgroundColor: String = "#FFFFFF"
+)

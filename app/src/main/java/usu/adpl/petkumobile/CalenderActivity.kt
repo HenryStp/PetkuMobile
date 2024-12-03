41e273b844b70b20c533e0fcda9228b586ac6d3e
henrystp <henrystp05@gmail.com>
2024-12-03 16:16:37 +0700

    memperbaiki error merge

--- app/src/main/java/usu/adpl/petkumobile/CalenderActivity.kt
+++ app/src/main/java/usu/adpl/petkumobile/CalenderActivity.kt
@@ -2,7 +2,6 @@ package usu.adpl.petkumobile
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
